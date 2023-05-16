1c6844bf3d5a54eede6ad0e6d124ffa2df352841
Vivek Nair <vivek@pesto.app>
2023-05-16 07:46:27 -0400

    fix: ensure that tests are working (#9)

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -29,14 +29,14 @@ describe('Execution tests', () => {
   });
 
   it('No parameters provided', async () => {
-    const result = await execute('node lib/cli.js migrate', '.');
+    const result = await execute('node lib/src/cli.js migrate', '.');
 
     expect(result.stderr).toBe("error: required option '--host <name>' not specified\n");
   });
 
   it('No migration directory', async () => {
     const command =
-      "node ./lib/cli.js  migrate --host=http://sometesthost:8123 --user=default --password='' --db=analytics --migrations-home=/app/clickhouse/migrations";
+      "node ./lib/src/cli.js  migrate --host=http://sometesthost:8123 --user=default --password='' --db=analytics --migrations-home=/app/clickhouse/migrations";
 
     const result = await execute(command, '.');
 
@@ -46,7 +46,7 @@ describe('Execution tests', () => {
   });
 
   it('Environment variables are provided, but no migration directory', async () => {
-    const result = await execute('node lib/cli.js migrate', { env: envVars });
+    const result = await execute('node lib/src/cli.js migrate', { env: envVars });
 
     expect(result.stderr).toBe(
       '\x1B[36m clickhouse-migrations : \x1B[31m Error: no migration directory /app/clickhouse/migrations. Please create it. \n',
@@ -55,7 +55,7 @@ describe('Execution tests', () => {
 
   it('Incorrectly named migration', async () => {
     const command =
-      "node ./lib/cli.js  migrate --host=http://sometesthost:8123 --user=default --password='' --db=analytics --migrations-home=tests/migrations/bad";
+      "node ./lib/src/cli.js  migrate --host=http://sometesthost:8123 --user=default --password='' --db=analytics --migrations-home=tests/migrations/bad";
 
     const result = await execute(command, '.');
 
