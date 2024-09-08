d288166dc59b1422a573a01d37c3159cd4641719
VVVi <728466-viktorv@users.noreply.gitlab.com>
2024-09-08 11:21:19 +0200

    feat: add tests workflow

--- src/migrate.ts
+++ src/migrate.ts
@@ -257,7 +257,7 @@ const migration = async (
 const migrate = () => {
   const program = new Command();
 
-  program.name('clickhouse-migrations').description('ClickHouse migrations.').version('0.2.2');
+  program.name('clickhouse-migrations').description('ClickHouse migrations.').version('1.0.0');
 
   program
     .command('migrate')
